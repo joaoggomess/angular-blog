47a44d3895fa6ff670a774f35f89b4b1ba86dc36
joao gomes <joao_rgomes@hotmail.com>
2022-11-18 10:51:28 -0300

    Base structure for the page

---
+++ src/app/components/small-card/small-title.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-small-card',
+  templateUrl: './small-title.component.html',
+  styleUrls: ['./small-title.component.css']
+})
+export class SmallCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
